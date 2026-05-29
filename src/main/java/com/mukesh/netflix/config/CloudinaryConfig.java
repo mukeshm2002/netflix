53294e4633d4c457015bab10ae30292674515bcb
Mukesh <mukeshkannan509@gmail.com>
2026-05-29 12:12:45 +0530

    Config update

---
+++ src/main/java/com/mukesh/netflix/config/CloudinaryConfig.java
@@ -0,0 +1,32 @@
+package com.mukesh.netflix.config;
+
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${dlm2b9qqr}")
+    private String cloudName;
+
+    @Value("${319386169641156}")
+    private String apiKey;
+
+    @Value("${o-PKKKzbj9Z2dINq0sTnvwRb8FE}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
+    }
+}
